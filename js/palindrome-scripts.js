fb9dda74adb4427cb15cda9aef07fd098ccb918e
Noah Ramey and Sebastian Prokuski <noah@>
2016-06-07 16:06:16 -0700

    add functional spaceless input conditions to palindrome js.

--- js/palindrome-scripts.js
+++ js/palindrome-scripts.js
@@ -1,14 +1,58 @@
+// // LEAVING SPACES IN
+
+// $(document).ready(function() {
+//   $("#main form").submit(function(event) {
+//     event.preventDefault();
+//
+//     var phrase = $("#phrase").val();
+//
+//     function reverse(x) {
+//       return x.split('').reverse().join('');
+//     }
+//
+//     var phraseFlip = reverse(phrase);
+//
+//     function palindromeChecker(x, y) {
+//       if (x === "") {
+//         alert("Please enter a word or sentence")
+//       } else
+//       if (x === y) {
+//         alert("That's a palindrome!")
+//       } else {
+//         alert("That's NOT a palindrome!")
+//       }
+//     };
+//
+//     palindromeChecker(phrase, phraseFlip);
+//   });
+// });
+
+// TAKING OUT SPACES
+
 $(document).ready(function() {
   $("#main form").submit(function(event) {
     event.preventDefault();
 
     var phrase = $("#phrase").val();
+    var phraseSpaceless = phrase.split('');
 
-    function reverse(x) {
-      return x.split('').reverse().join('');
-    }
+    // alert(phraseSpaceless); --> array is split into characters
 
-    var phraseFlip =  reverse(phrase);
+    phraseSpaceless = phraseSpaceless.filter(function(x){
+      return /\S/.test(x);
+    });
+
+    alert(phraseSpaceless);
+    // --> array is split into characters, spaces removed
+
+    var phraseSpacelessFlip = phraseSpaceless.reverse().join('');
+    phraseSpaceless = phraseSpaceless.reverse().join('');
+
+    alert(phraseSpacelessFlip);
+    // --> array is reversed and joined
+    alert(phraseSpaceless);
+    // --> array is reversed twice and joined
+
 
     function palindromeChecker(x, y) {
       if (x === "") {
@@ -21,6 +65,9 @@ $(document).ready(function() {
       }
     };
 
-    palindromeChecker(phrase, phraseFlip);
+    alert(phraseSpacelessFlip);
+    alert(phraseSpaceless);
+
+    palindromeChecker(phraseSpaceless, phraseSpacelessFlip);
   });
 });
---
+++ js/prime-scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $("#main form").submit(function(event) {
+    event.preventDefault();
+
+    var number = parseInt($("#number").val());
+    var newArray = [];
+    var newestArray
+    var total = 0;
+
+    for (var i = 2; i <= number; i++) {
+      newArray.push([i]);
+    }
+
+    if (newArray[i]*newArray[i] <= number)
+      
+
+    var newFunction = function
+    for (var i = 2; y <= number; y=i*2) {
+      newestArray.push([i]);
+    }
+
+
+    alert(newestArray);
+
+
+    });
+});
+
+
+// if (i*i <= number) {
+//   i*i;
+// }
+// if (i*i*i <= number) {
+//   i*i*i;
+// }
+// if (i*i*i*i <= number) {
+//   i*i*i*i;
+// }
